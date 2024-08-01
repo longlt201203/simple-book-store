668df2d7bd0c0b7d426563da9fecec386d6282ca
longthanhle1122 <longlt201203@gmail.com>
2024-08-01 15:57:56 +0700

    Initial commit

---
+++ src/utils/env.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+
+config();
+
+export const Env = {
+	LISTEN_PORT: parseInt(process.env.LISTEN_PORT || "3000"),
+
+	DB_HOST: process.env.DB_HOST || "localhost",
+	DB_PORT: parseInt(process.env.DB_PORT || ""),
+	DB_NAME: process.env.DB_NAME || "",
+	DB_USER: process.env.DB_USER || "",
+	DB_PASS: process.env.DB_PASS || "",
+} as const;
+
+console.log(Env);
